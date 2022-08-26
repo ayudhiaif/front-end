eb0c3b2c078f7e7fd40b4354f314d9f0cd7c7bc3
Ayudhia Isnafiani Fanada <48143975+ayudhiaif@users.noreply.github.com>
2022-08-26 09:41:19 +0700

    Add files via upload

---
+++ depart.js
@@ -0,0 +1,5 @@
+console.log("Selamat Anda berhasil menggunakan JavaScript pada Website");
+
+catImage.addEventListener('click', function(event) {
+   document.querySelector('#count').innerText++;
+});
